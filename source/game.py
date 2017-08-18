cb276f16d0a09978e2698301da81fafd4fc68a99
olofs <svedvall@gmail.com>
2017-08-18 09:54:41 +0200

    Refactored. Skapade en klass Box. Alla som ärver Box får en position, hastighet samt dimensioner. Klassen Box är en enkel representation av objekten i spelet för att objekten ska kunna interagera med varandra.

--- source/game.py
+++ source/game.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-class Ball:
+
+class Box:
     def __init__(self):
         self.position = np.array([0, 0])
         self.velocity = np.array([1, 1])
-        self.radie = 1
+        self.dimensions = np.array([1, 1])
+
+class Ball(Box):
+    def __init__(self):
+        super(Ball, self).__init__()
 
     def move(self, dt):
         self.position += self.velocity * dt
