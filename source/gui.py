b39e3ebc9655c13f9a864bf4cc8eb541917d2f03
olofs <svedvall@gmail.com>
2017-08-18 13:23:16 +0200

    Skapat widgets kontainer Menu samt knapparna Start, Stop, Paus, Restart.

--- source/gui.py
+++ source/gui.py
@@ -1,7 +1,48 @@
 import tkinter as tk
 
 
+
+
+class PongArea:
+    pass
+
+
+class Menu(tk.Frame):
+    def __init__(self, master, **kwargs):
+        super(Menu, self).__init__(master, **kwargs)
+        self.buttons = [Stop(master, text='Stop', command=None), Start(master, text='Start', command=None) \
+                        , Paus(master, text='Paus', command=None), Restart(master, text='Restart', command=None)]
+        for button in self.buttons:
+            button.pack()
+
+
+class Button(tk.Button):
+    def __init__(self, master, **kwargs):
+        super(Button, self).__init__(master, **kwargs)
+
+
+class Start(Button):
+    def __init__(self, master, **kwargs):
+        super(Start, self).__init__(master, **kwargs)
+
+
+class Stop(Button):
+    def __init__(self, master, **kwargs):
+        super(Stop, self).__init__(master, **kwargs)
+
+
+class Paus(Button):
+    def __init__(self, master, **kwargs):
+        super(Paus, self).__init__(master, **kwargs)
+
+
+class Restart(Button):
+    def __init__(self, master, **kwargs):
+        super(Restart, self).__init__(master, **kwargs)
+
+
 top = tk.Tk()
-# lägg till widgets här
+# lägg till frames här
+menu = Menu(top)
 
 top.mainloop()
